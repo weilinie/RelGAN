fix some gradient issues

models/rmc_vdcnn.py
@@ -13,11 +13,13 @@ cnn_initializer = tf.keras.initializers.he_normal()
 fc_initializer = tf.truncated_normal_initializer(stddev=0.05)
 
 
+# The generator network based on the Relational Memory
 def generator(x_real, temperature, vocab_size, batch_size, seq_len, gen_emb_dim, mem_slots, head_size, num_heads,
               hidden_dim, start_token):
     start_tokens = tf.constant([start_token] * batch_size, dtype=tf.int32)
     output_size = mem_slots * head_size * num_heads
 
+    # build relation memory module
     g_embeddings = tf.get_variable('g_emb', shape=[vocab_size, gen_emb_dim],
                                    initializer=create_linear_initializer(vocab_size))
     gen_mem = RelationalMemory(mem_slots=mem_slots, head_size=head_size, num_heads=num_heads)
@@ -30,22 +32,29 @@ def generator(x_real, temperature, vocab_size, batch_size, seq_len, gen_emb_dim,
     gen_o = tensor_array_ops.TensorArray(dtype=tf.float32, size=seq_len, dynamic_size=False, infer_shape=True)
     gen_x = tensor_array_ops.TensorArray(dtype=tf.int32, size=seq_len, dynamic_size=False, infer_shape=True)
     gen_x_onehot_adv = tensor_array_ops.TensorArray(dtype=tf.float32, size=seq_len, dynamic_size=False,
-                                                    infer_shape=True)
+                                                    infer_shape=True)  # generator output (relaxed of gen_x)
 
+    # the generator recurrent module used for adversarial training
     def _gen_recurrence(i, x_t, h_tm1, gen_o, gen_x, gen_x_onehot_adv):
         mem_o_t, h_t = gen_mem(x_t, h_tm1)  # hidden_memory_tuple
-        o_t = g_output_unit(mem_o_t)  # batch x vocab, logits not prob
+        o_t = g_output_unit(mem_o_t)  # batch x vocab, logits not probs
         gumbel_t = add_gumbel(o_t)
-        next_token = tf.cast(tf.argmax(gumbel_t, axis=1), tf.int32)
+        next_token = tf.stop_gradient(tf.argmax(gumbel_t, axis=1, output_type=tf.int32))
+        next_token_onehot = tf.one_hot(next_token, vocab_size, 1.0, 0.0)
+
         x_onehot_appr = tf.nn.softmax(tf.multiply(gumbel_t, temperature))  # one-hot-like, [batch_size x vocab_size]
+
         # x_tp1 = tf.matmul(x_onehot_appr, g_embeddings)  # approximated embeddings, [batch_size x emb_dim]
         x_tp1 = tf.nn.embedding_lookup(g_embeddings, next_token)  # embeddings, [batch_size x emb_dim]
-        gen_o = gen_o.write(i, tf.reduce_sum(tf.multiply(tf.one_hot(next_token, vocab_size, 1.0, 0.0),
-                                                         tf.nn.softmax(o_t)), 1))  # [batch_size] , prob
+
+        gen_o = gen_o.write(i, tf.reduce_sum(tf.multiply(next_token_onehot, x_onehot_appr), 1))  # [batch_size], prob
         gen_x = gen_x.write(i, next_token)  # indices, [batch_size]
+
         gen_x_onehot_adv = gen_x_onehot_adv.write(i, x_onehot_appr)
+
         return i + 1, x_tp1, h_t, gen_o, gen_x, gen_x_onehot_adv
 
+    # build a graph for outputting sequential tokens
     _, _, _, gen_o, gen_x, gen_x_onehot_adv = control_flow_ops.while_loop(
         cond=lambda i, _1, _2, _3, _4, _5: i < seq_len,
         body=_gen_recurrence,

oracle/oracle_gan/oracle_train.py
@@ -56,15 +56,14 @@ def oracle_train(generator, discriminator, oracle_model, oracle_loader, gen_load
 
     # GAN / Divergence type
     log_pg, g_loss, d_loss = get_losses(d_out_real, d_out_fake, x_real_onehot, x_fake_onehot_appr,
-                                                    gen_o, discriminator, config)
+                                        gen_o, discriminator, config)
 
     # Global step
     global_step = tf.Variable(0, trainable=False)
     global_step_op = global_step.assign_add(1)
 
     # Train ops
-    g_pretrain_op, g_train_op, d_train_op, temp_train_op = get_train_ops(config, g_pretrain_loss, g_loss, d_loss,
-                                                                         log_pg, temperature, global_step)
+    g_pretrain_op, g_train_op, d_train_op = get_train_ops(config, g_pretrain_loss, g_loss, d_loss, global_step)
 
     # Record wall clock time
     time_diff = tf.placeholder(tf.float32)
@@ -131,7 +130,7 @@ def oracle_train(generator, discriminator, oracle_model, oracle_loader, gen_load
                 log.write('\n')
 
                 # save the model
-                saver.save(sess, os.path.join(log_dir,  'ckpt', 'oracle.pre_model'), global_step=epoch)
+                saver.save(sess, os.path.join(log_dir, 'ckpt', 'oracle.pre_model'), global_step=epoch)
 
         print('Start adversarial training...')
         progress = tqdm(range(nadv_steps))
@@ -183,7 +182,7 @@ def oracle_train(generator, discriminator, oracle_model, oracle_loader, gen_load
                 log.write('\n')
 
                 # save the model
-                saver.save(sess, os.path.join(log_dir,  'ckpt', 'oracle.adv_model'), global_step=global_step)
+                saver.save(sess, os.path.join(log_dir, 'ckpt', 'oracle.adv_model'), global_step=global_step)
 
 
 # A function to get different GAN losses
@@ -237,14 +236,14 @@ def get_losses(d_out_real, d_out_fake, x_real_onehot, x_fake_onehot_appr, gen_o,
         d_loss = tf.reduce_mean(tf.tanh(d_out_fake) - tf.tanh(d_out_real))
         g_loss = tf.reduce_mean(-tf.tanh(d_out_fake))
 
-    elif gan_type == 'wgan-gp': # WGAN-GP
+    elif gan_type == 'wgan-gp':  # WGAN-GP
         d_loss = tf.reduce_mean(d_out_fake) - tf.reduce_mean(d_out_real)
         GP = gradient_penalty(discriminator, x_real_onehot, x_fake_onehot_appr, config)
         d_loss += GP
 
         g_loss = -tf.reduce_mean(d_out_fake)
 
-    elif gan_type == 'LS': # LS-GAN
+    elif gan_type == 'LS':  # LS-GAN
         d_loss_real = tf.reduce_mean(tf.squared_difference(d_out_real, 1.0))
         d_loss_fake = tf.reduce_mean(tf.square(d_out_fake))
         d_loss = d_loss_real + d_loss_fake
@@ -268,7 +267,7 @@ def get_losses(d_out_real, d_out_fake, x_real_onehot, x_fake_onehot_appr, gen_o,
 
 
 # A function to calculate the gradients and get training operations
-def get_train_ops(config, g_pretrain_loss, g_loss, d_loss, log_pg, temperature, global_step):
+def get_train_ops(config, g_pretrain_loss, g_loss, d_loss, global_step):
     optimizer_name = config['optimizer']
     nadv_steps = config['nadv_steps']
     d_lr = config['d_lr']
@@ -292,28 +291,24 @@ def get_train_ops(config, g_pretrain_loss, g_loss, d_loss, log_pg, temperature, 
     if optimizer_name == 'adam':
         d_optimizer = tf.train.AdamOptimizer(d_lr, beta1=0.9, beta2=0.999)
         g_optimizer = tf.train.AdamOptimizer(gadv_lr, beta1=0.9, beta2=0.999)
-        temp_optimizer = tf.train.AdamOptimizer(1e-2, beta1=0.9, beta2=0.999)
     elif optimizer_name == 'rmsprop':
         d_optimizer = tf.train.RMSPropOptimizer(d_lr)
         g_optimizer = tf.train.RMSPropOptimizer(gadv_lr)
-        temp_optimizer = tf.train.RMSPropOptimizer(1e-2)
     else:
         raise NotImplementedError
 
+    # gradient clipping
     g_grads, _ = tf.clip_by_global_norm(tf.gradients(g_loss, g_vars), grad_clip)  # gradient clipping
     g_train_op = g_optimizer.apply_gradients(zip(g_grads, g_vars))
 
     print('len of g_grads without None: {}'.format(len([i for i in g_grads if i is not None])))
     print('len of g_grads: {}'.format(len(g_grads)))
 
-    # d_train_op = d_optimizer.minimize(d_loss, var_list=d_vars)
+    # gradient clipping
     d_grads, _ = tf.clip_by_global_norm(tf.gradients(d_loss, d_vars), grad_clip)  # gradient clipping
     d_train_op = d_optimizer.apply_gradients(zip(d_grads, d_vars))
 
-    temp_grads = tf.gradients(-log_pg, [temperature])
-    temp_train_op = temp_optimizer.apply_gradients(zip(temp_grads, [temperature]))
-
-    return g_pretrain_op, g_train_op, d_train_op, temp_train_op
+    return g_pretrain_op, g_train_op, d_train_op
 
 
 # A function to get various evaluation metrics
@@ -371,7 +366,7 @@ def get_fixed_temperature(temper, i, nadv_steps, adapt):
     elif adapt == 'sigmoid':
         temper_var_np = (temper - 1) * 1 / (1 + np.exp((N / 2 - i) * 20 / N)) + 1  # sigmoid increase
     elif adapt == 'quad':
-        temper_var_np = (temper - 1) / (N - 1)**2 * i ** 2 + 1
+        temper_var_np = (temper - 1) / (N - 1) ** 2 * i ** 2 + 1
     elif adapt == 'sqrt':
         temper_var_np = (temper - 1) / np.sqrt(N - 1) * np.sqrt(i) + 1
     else:

real/real_gan/real_train.py
@@ -63,15 +63,14 @@ def real_train(generator, discriminator, oracle_loader, config):
 
     # GAN / Divergence type
     log_pg, g_loss, d_loss = get_losses(d_out_real, d_out_fake, x_real_onehot, x_fake_onehot_appr,
-                                                    gen_o, discriminator, config)
+                                        gen_o, discriminator, config)
 
     # Global step
     global_step = tf.Variable(0, trainable=False)
     global_step_op = global_step.assign_add(1)
 
     # Train ops
-    g_pretrain_op, g_train_op, d_train_op = get_train_ops(config, g_pretrain_loss, g_loss, d_loss,
-                                                          log_pg, temperature, global_step)
+    g_pretrain_op, g_train_op, d_train_op = get_train_ops(config, g_pretrain_loss, g_loss, d_loss, global_step)
 
     # Record wall clock time
     time_diff = tf.placeholder(tf.float32)
@@ -274,7 +273,7 @@ def get_losses(d_out_real, d_out_fake, x_real_onehot, x_fake_onehot_appr, gen_o,
 
 
 # A function to calculate the gradients and get training operations
-def get_train_ops(config, g_pretrain_loss, g_loss, d_loss, log_pg, temperature, global_step):
+def get_train_ops(config, g_pretrain_loss, g_loss, d_loss, global_step):
     optimizer_name = config['optimizer']
     nadv_steps = config['nadv_steps']
     d_lr = config['d_lr']
@@ -300,13 +299,11 @@ def get_train_ops(config, g_pretrain_loss, g_loss, d_loss, log_pg, temperature, 
     if optimizer_name == 'adam':
         d_optimizer = tf.train.AdamOptimizer(d_lr, beta1=0.9, beta2=0.999)
         g_optimizer = tf.train.AdamOptimizer(gadv_lr, beta1=0.9, beta2=0.999)
-        temp_optimizer = tf.train.AdamOptimizer(1e-2, beta1=0.9, beta2=0.999)
 
     # RMSProp optimizer
     elif optimizer_name == 'rmsprop':
         d_optimizer = tf.train.RMSPropOptimizer(d_lr)
         g_optimizer = tf.train.RMSPropOptimizer(gadv_lr)
-        temp_optimizer = tf.train.RMSPropOptimizer(1e-2)
 
     else:
         raise NotImplementedError
@@ -394,7 +391,7 @@ def get_fixed_temperature(temper, i, nadv_steps, adapt):
     elif adapt == 'sigmoid':
         temper_var_np = (temper - 1) * 1 / (1 + np.exp((N / 2 - i) * 20 / N)) + 1  # sigmoid increase
     elif adapt == 'quad':
-        temper_var_np = (temper - 1) / (N - 1)**2 * i ** 2 + 1
+        temper_var_np = (temper - 1) / (N - 1) ** 2 * i ** 2 + 1
     elif adapt == 'sqrt':
         temper_var_np = (temper - 1) / np.sqrt(N - 1) * np.sqrt(i) + 1
     else: